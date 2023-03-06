2b530f074d52625bc1c0983c12d538e75282d62a
Dmitriy Bachromov <dvbak@yandex.ru>
2023-03-06 16:44:49 +0300

    fixed js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 const popUp = document.querySelector('.popup');
 const btnEdit = document.querySelector('.profile__btn-edit');
-const btnClose = document.querySelector('.popup__closed');
+const btnClose = popUp.querySelector('.popup__closed');
 
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
-let formPopup = document.querySelector('.popup__form');
-let inputName = document.querySelector('.popup__input-name');
-let inputAbout = document.querySelector('.popup__input-about');
+let formPopup = popUp.querySelector('.popup__form');
+let inputName = document.querySelector('.popup__input_name_name');
+let inputAbout = document.querySelector('.popup__input_name_about');
+// Пока не понял для чего вводить дополнительно модификаторы, которые предназначены только для JS. В данном случае можно просто определить массив инпутов и работать с каждым элементом в отдельности ( см. закомментированный код).
+// let inputText = popUp.querySelectorAll('.popup__input');
 
 btnEdit.addEventListener('click', openPopUp);
 btnClose.addEventListener('click', closePopUp);
@@ -14,26 +16,19 @@ formPopup.addEventListener('submit', handleFormSubmit);
 
 function openPopUp() {
   popUp.classList.add('popup_opened');
-  inputName.placeholder = profileName.textContent;
-  inputAbout.placeholder = profileAbout.textContent;
-  // Вывод значений в попапе через пласхолдер удлиняет код на две строки, но при этом получаем удобства в заполнении формы (не надо предварительно удалять текст из полей ввода). Да и по дизайну (на мой взгляд) всплывающее окно смотрится лучше.
-  // inputName.value = profileName.textContent;
-  // inputAbout.value = profileAbout.textContent;
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  // inputText[0].value = profileName.textContent;
+  // inputText[1].value = profileAbout.textContent;
 }
 
 function closePopUp() {
   popUp.classList.remove('popup_opened');
-  inputName.value = '';
-  inputAbout.value = '';
 }
 
 function handleFormSubmit(event) {
   event.preventDefault();
-
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-
-  popUp.classList.remove('popup_opened');
-  inputName.value = '';
-  inputAbout.value = '';
+  profileName.textContent = inputName.value; //inputText[0].value
+  profileAbout.textContent = inputAbout.value; //inputText[1].value
+  closePopUp();
 }
