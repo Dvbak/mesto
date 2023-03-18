8571d67526e19e2980258da5a5ce9eb6dbdd33ce
Dmitriy Bachromov <dvbak@yandex.ru>
2023-03-18 23:37:37 +0300

    add popup img

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const popUpEdit = document.querySelector('.popup_edit');
 const btnEdit = document.querySelector('.profile__btn-edit');
 const popUpAdd = document.querySelector('.popup_add');
 const btnAdd = document.querySelector('.profile__btn-add');
+const popUpImg = document.querySelector('.popup_img');
 const buttonsClose = document.querySelectorAll('.popup__closed');
 
 let profileName = document.querySelector('.profile__title');
@@ -13,28 +14,22 @@ let formPopupAdd = popUpAdd.querySelector('.popup__form');
 let inputPlace = popUpAdd.querySelector('.popup__input_name_place');
 let inputLink = popUpAdd.querySelector('.popup__input_name_link');
 
-btnEdit.addEventListener('click', function() {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-  openPopUp(popUpEdit)});
-btnAdd.addEventListener('click', () => openPopUp(popUpAdd));
-formPopupEdit.addEventListener('submit', editFormSubmit);
-formPopupAdd.addEventListener('submit', creatCardFormSubmit);
-
-buttonsClose.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    const popup = btn.closest('.popup');
-    closePopUp(popup);
-  });
-});
-
 function openPopUp(modal) {
   modal.classList.add('popup_opened');
 }
-
 function closePopUp(modal) {
   modal.classList.remove('popup_opened');
 }
+btnEdit.addEventListener('click', function() {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  openPopUp(popUpEdit);
+});
+btnAdd.addEventListener('click', function() {
+  openPopUp(popUpAdd);
+});
+
+formPopupEdit.addEventListener('submit', editFormSubmit);
 
 function editFormSubmit(event) {
   event.preventDefault();
@@ -43,6 +38,8 @@ function editFormSubmit(event) {
   closePopUp(popUpEdit);
 }
 
+formPopupAdd.addEventListener('submit', creatCardFormSubmit);
+
 function creatCardFormSubmit(event) {
   event.preventDefault();
   creatCard(inputPlace.value, inputLink.value);
@@ -50,6 +47,13 @@ function creatCardFormSubmit(event) {
   inputLink.value = '';
   closePopUp(popUpAdd);
 }
+
+buttonsClose.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    const popup = btn.closest('.popup');
+    closePopUp(popup);
+  });
+});
 
 const initialCards = [
   {
@@ -79,34 +83,37 @@ const initialCards = [
 ];
 const cardsGrid = document.querySelector('.elements__grid');
 const cardTemplate = cardsGrid.querySelector('#card').content;
-console.log(cardTemplate);
 
 function creatCard(name, link) {
   let clonTemplateCard = cardTemplate.querySelector('.elements__item').cloneNode(true);
   clonTemplateCard.querySelector('.elements__img').src = link;
   clonTemplateCard.querySelector('.elements__img').alt = name;
   clonTemplateCard.querySelector('.elements__title').textContent = name;
+
   let btnAddLike = clonTemplateCard.querySelector('.elements__btn');
-  btnAddLike.addEventListener('click', () => btnAddLike.classList.toggle('elements__btn_like'));
+  btnAddLike.addEventListener('click', function() {
+    btnAddLike.classList.toggle('elements__btn_like');
+  });
   let btnCardDelet = clonTemplateCard.querySelector('.elements__btn-delet');
   btnCardDelet.addEventListener('click', function() {
-    const elementsItem = btnCardDelet.closest('.elements__item');
-    elementsItem.remove();
+    btnCardDelet.closest('.elements__item').remove();
+  });
+  let btnOpenImg = clonTemplateCard.querySelector('.elements__img');
+  btnOpenImg.addEventListener('click', function() {
+    popUpImg.querySelector('.popup__image').src = link;
+    popUpImg.querySelector('.popup__caption').textContent = name;
+    openPopUp(popUpImg);
   });
   cardsGrid.prepend(clonTemplateCard);
 }
 
-for (let i = initialCards.length - 1; i >= 0; i--) {
-  creatCard(initialCards[i].name, initialCards[i].link);
-} // Запускаю обратный цикл, т.к. по заданию карточки надо вставлять в начало списка (prepend), а порядок следования в массиве не соответсвует описанию в задании. Можно сделать и через свойства массивов(см. ниже)
+initialCards.slice().reverse().forEach(function(item) {
+creatCard(item.name, item.link);
+}); // Запускаю обратный массив, т.к. по заданию карточки надо вставлять в начало списка (prepend), а порядок следования в массиве не соответсвует описанию в задании. Можно сделать и через обратный цикл(см. ниже).
 
-// initialCards.slice().reverse().forEach(function(item) {
-// creatCard(item.name, item.link);
-// });
+// for (let i = initialCards.length - 1; i >= 0; i--) {
+//   creatCard(initialCards[i].name, initialCards[i].link);
+// }
 
-const buttonsLike = document.getElementsByClassName('elements__btn');
-console.log(buttonsLike);
 
-const buttonsDelet = document.querySelectorAll('.elements__btn-delet');
-console.log(buttonsDelet);
 
